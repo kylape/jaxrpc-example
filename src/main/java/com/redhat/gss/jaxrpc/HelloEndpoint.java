4cbaed0a3002b7af9ebf9d73f16ec841259e5812
Kyle Lape <kyle.lape@redhat.com>
2014-05-15 15:37:06 -0700

    Adding last bit to deploy complete app to EAP 6

--- src/main/java/com/redhat/gss/jaxrpc/HelloEndpoint.java
+++ src/main/java/com/redhat/gss/jaxrpc/HelloEndpoint.java
@@ -2,9 +2,11 @@ package com.redhat.gss.jaxrpc;
 
 import java.rmi.RemoteException;
 
+import org.jboss.logging.Logger;
+
 public class HelloEndpoint implements HelloWorld
 {
-  private static Logger log = Logger.getLogger(HelloEndpoint.class;
+  private static Logger log = Logger.getLogger(HelloEndpoint.class);
 
   public String sayHello(String name) throws RemoteException
   {
